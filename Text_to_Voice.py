8bb426e63e21574ef3319cd6ecc2e5884f481aeb
Sergio Huapaya Llimper <sergio.huapaya0704@gmail.com>
2026-02-19 14:40:29 -0500

    Add files via upload

---
+++ Text_to_Voice.py
@@ -0,0 +1,48 @@
+import os
+from tkinter import Tk, Text, Button, END, Label
+from datetime import datetime
+from gtts import gTTS
+
+#Sergio Rodrigo Huapaya Llimper
+
+def save_text():
+    text = text_area.get('1.0', END)
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    filename = f"text_{timestamp}.txt"
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    status_label.config(text=f'Texto guardado como {filename}')
+
+def text_to_voice():
+    files = [f for f in os.listdir() if f.startswith("text_") and f.endswith(".txt")]
+    if not files:
+        status_label.config(text="No hay archivos de texto guardados")
+        return
+    latest_txt = max(files, key=os.path.getmtime)
+    with open(latest_txt, 'r', encoding='utf-8') as file:
+        text = file.read()
+    mp3_filename = latest_txt.replace(".txt", ".mp3")
+    speech = gTTS(text=text, lang='es', slow=False)
+    speech.save(mp3_filename)
+    os.system(f'start {mp3_filename}')
+    status_label.config(text=f'Reproduciento texto de {mp3_filename}')
+
+#Creación de ventana principal
+root = Tk()
+root.title('Texto a Voz')
+
+text_area = Text(root, height=10, width=50)
+text_area.pack()
+
+save_button = Button(root, text='Guardar Texto', command=save_text)
+save_button.pack()
+
+play_button = Button(root, text='Reproducir Texto', command=text_to_voice)
+play_button.pack()
+
+status_label = Label(root, text="" , fg="green")
+status_label.pack()
+
+root.mainloop()
+
+root.mainloop()
